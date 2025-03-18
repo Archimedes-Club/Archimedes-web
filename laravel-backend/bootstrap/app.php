b5d3bc7ac9ae4541c25101c5727ec62acbcbbf8e
Abhinav-1502 <eeranti.a@northeastern.edu>
2025-03-18 15:23:32 -0400

    User authentication using cookies
    Changed the authentication from token based to cookie session

--- laravel-backend/bootstrap/app.php
+++ laravel-backend/bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\EnsureAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +15,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class, 
+        ]);
+
+        $middleware->alias([
+            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        ]);
+
         $middleware->alias([
             'auth.admin' => EnsureAdmin::class,
+        ]);
+        $middleware->group('api', [
+            EnsureFrontendRequestsAreStateful::class,
+            StartSession::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
